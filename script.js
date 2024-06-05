f1b742d480d2a287a66b3f24b1b154aa0e64d1dc
ashleychinny <ashleychin@oakland.edu>
2024-06-05 18:35:39 -0400

    first draft

---
+++ script.js
@@ -0,0 +1,12 @@
+const usedCars = [
+  {
+    year: 2018,
+    make: "Toyota",
+    model: "Camry",
+    mileage: 30000,
+    price: 18000,
+    color: "Silver",
+    gasMileage: "25 mpg city, 35 mpg highway",
+    image: "images/Camry,toyota.webp",
+  },
+]
